21efb5b5247742f61a9125b546140002ca500072
soccerKevin <soccerkevint11@gmail.com>
2015-04-15 17:14:26 -0700

    jewel test get by id

--- test/jewel_test.rb
+++ test/jewel_test.rb
@@ -3,11 +3,20 @@ require 'test_helper'
 class JewelApiTest < ActionDispatch::IntegrationTest
   setup { host! 'api.gemstore.com' }
 
-  test 'returns list of gems' do
+  test 'as a user, I want to get all the gems' do
     get '/jewels'
     assert_equal 200, response.status
     refute_empty response.body
   end
 
+  test 'as a user, I want to get a jewel by an id' do
+    jewel = Jewel.create!(name: 'RubyGem', price: 10.20)
+    get "/jewels/#{jewel.id}"
+    assert_equal 200, response.status
+
+    jewel_response = json(response.body)
+    assert_equal jewel.name, jewel_response[:name]
+    assert_equal jewel.price.to_s, jewel_response[:price].to_s
+  end
   
 end
