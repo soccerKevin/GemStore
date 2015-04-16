e9155ec8116529bf763e414ad604a7cf42841794
soccerKevin <soccerkevint11@gmail.com>
2015-04-15 17:14:40 -0700

    jewel get by id works

--- app/controllers/api/jewels_controller.rb
+++ app/controllers/api/jewels_controller.rb
@@ -1,9 +1,13 @@
-module API
-  class JewelsController < ApplicationController
-    def index
-      jewels = Jewel.all
+class API::JewelsController < ApplicationController
+  def index
+    jewels = Jewel.all
 
-      render json: jewels, status: 200
-    end
+    render json: jewels, status: :ok
+  end
+
+  def show
+    jewel = Jewel.find(params[:id])
+
+    render json: jewel, status: :ok
   end
 end
