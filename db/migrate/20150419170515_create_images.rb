5bcb44a23eea42865f47b6f50bf226dc35c48f18
kevin townsend <kevintownsend@kevins-MacBook-Pro.local>
2015-04-19 11:24:33 -0700

    migrations for images and reviews

---
+++ db/migrate/20150419170515_create_images.rb
@@ -0,0 +1,9 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+    	t.belongs_to :jewel, index: true
+    	t.string :url
+      t.timestamps null: false
+    end
+  end
+end
