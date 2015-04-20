586476f18257f02ccdbe22da03ce5373d65cd4dc
kevin townsend <kevintownsend@kevins-MacBook-Pro.local>
2015-04-19 23:28:24 -0700

    create a new gem at will!!!

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,27 @@
 (function() {
   var app = angular.module('gemStore', ['templates', 'store-directives']);
 
+  app.config(['$httpProvider', function($httpProvider){
+    $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
+  }]);
+
   app.controller('StoreController', ['$http', '$scope', function($http, $scope){
     $scope.products = [];
+    $scope.jewel = {};
 
     $http.get('/jewels')
       .success(function(data){
         $scope.products = data;
       });
+
+    $scope.create = function(crud){
+      $http.post('/jewels', $scope.jewel)
+        .success(function(data){
+          $scope.products.push(data)
+          $scope.jewel = {};
+          crud.setTab(crud.INDEX);
+        });
+    };
   }]);
 
   app.controller('ReviewController', function() {
